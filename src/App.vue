f3081130cab5f17e7bdc8483d2e5220250027f6c
Lss03 <2782776285@qq.com>
2023-12-26 16:04:06 +0800

    逐步优化

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
     <v-row class="fill-height ma-0">
       <!-- 固定侧边栏 -->
       <v-col cols="12" md="3" class="pa-0">
-        <v-navigation-drawer permanent app class="white">
-          <!-- 用户头像和昵称 -->
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <img src="./assets/logo.svg" alt="用户头像">
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>神秘人</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+        <!-- 使用内置的背景色 -->
+        <v-navigation-drawer permanent app color="blue-grey lighten-5">
+          <!-- 用户头像和昵称，使用v-list-item-group和v-list-item的slots -->
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <img src="./assets/logo.svg" alt="用户头像">
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>神秘人</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
           <v-divider></v-divider>
           <!-- 导航链接 -->
-          <v-list dense class="pt-0">
+          <v-list dense>
             <v-list-item link @click="$router.push('/')">
               <v-list-item-icon>
                 <v-icon>mdi-home</v-icon>
@@ -52,16 +55,26 @@
           </v-list>
         </v-navigation-drawer>
       </v-col>
-
       <!-- 右侧内容区 -->
-      <v-col cols="12" md="9">
-        <!-- 艺术字标题 -->
-        <div class="artistic-header">
-          <div class="artistic-title">艺景图片交易绘画平台</div>
-        </div>
-        <!-- 内容区域的padding -->
-        <v-container class="py-0 px-4">
-          <!-- 路由视图 -->
+      <v-col cols="12" md="9" class="pa-0">
+        <!-- 右侧区域的顶部标题 -->
+        <v-sheet
+            color="blue lighten-2"
+            class="d-flex align-center"
+            height="200px"
+            tile
+        >
+          <v-container>
+            <v-row justify="center">
+              <v-col cols="12" class="text-center">
+                <span class="display-2 white--text">艺景图片交易绘画平台</span>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-sheet>
+
+        <!-- 路由视图 -->
+        <v-container class="py-0 px-4 fill-height">
           <router-view></router-view>
         </v-container>
       </v-col>
@@ -75,47 +88,9 @@ export default {
   data: () => ({
     // Data properties here
   }),
-  // Methods or other options here
+  methods: {
+  },
+
+  // Other options here
 };
 </script>
-
-<style scoped>
-.artistic-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #1976D2; /* 或您喜欢的颜色 */
-  padding: 16px 0;
-  border-radius: 16px;
-  position: fixed; /* 将位置设置为固定 */
-  top: 0;
-  left: 300px; /* 与侧边栏的宽度相同 */
-  right: 0;
-  z-index: 100;
-}
-
-.artistic-title {
-  font-family: 'Amatic SC', cursive; /* 或您喜欢的艺术字体 */
-  font-size: 3rem;
-  color: #FFD700; /* 金色 */
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-}
-
-.content-area {
-  padding-top: 80px; /* 为固定的标题栏留出足够空间 */
-  overflow-y: auto;
-  height: 100vh;
-  padding-right: 16px; /* 防止内容紧贴右侧边缘 */
-}
-
-@media (min-width: 960px) {
-  .v-navigation-drawer {
-    width: 300px !important;
-  }
-
-  .artistic-header {
-    left: 300px; /* 保持与侧边栏宽度一致 */
-  }
-}
-</style>
-
--- src/components/goodsList.vue
+++ src/components/goodsList.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="list">
-    <div class="item" v-for="goods in goodslist" :key="goods.id">
-      <div class="item-l">
-        <img class="item-img" :src="goods.src">
-      </div>
-      <div class="item-r">
-        <div class="item-title">{{ goods.title }}</div>
-        <div class="item-price">{{ goods.price | currency }}</div>
-        <div class="item-opt">
-          <button @click="add(goods)">加入购物车</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+          cols="12"
+          v-for="goods in goodslist"
+          :key="goods.id"
+          class="my-3"
+      >
+        <v-card>
+          <v-layout row wrap>
+            <v-flex xs4>
+              <v-img :src="goods.src" class="grey lighten-2" aspect-ratio="1" ></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-title primary-title>
+                {{ goods.title }}
+              </v-card-title>
+              <v-card-subtitle>
+                {{ goods.price | currency }}
+              </v-card-subtitle>
+              <v-card-actions>
+                <v-btn color="coral" @click="add(goods)">加入购物车</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -34,63 +49,7 @@ export default {
 }
 </script>
 
+<!-- 由于现在使用了 Vuetify 的内建类，所以不再需要大量的自定义 CSS -->
 <style>
-/* 商品列表样式 */
-.list {
-  padding-left: -48px; /* 左边距 */
-  padding-top: 96px; /* 顶部内边距，根据标题栏的高度进行调整 */
-  margin-left: -96px; /* 与侧边栏宽度一致的外边距 */
-}
-
-.item {
-  border-bottom: 1px dashed #ccc;
-  padding: 10px;
-  margin-bottom: 16px; /* 商品项底部间距 */
-}
-
-.item::after {
-  content: "";
-  display: block;
-  clear: both;
-}
-
-.item-l {
-  float: left;
-  font-size: 0;
-}
-
-.item-img {
-  width: 100px;
-  height: 100px;
-  border: 1px solid #ccc;
-}
-
-.item-r {
-  float: left;
-  padding-left: 20px; /* 商品信息左边距 */
-}
-
-.item-title {
-  font-size: 14px;
-  margin-top: 10px;
-}
-
-.item-price {
-  margin-top: 10px;
-  color: #f00;
-  font-size: 15px;
-}
-
-.item-opt {
-  margin-top: 10px;
-}
-
-.item-opt button {
-  border: 0;
-  background: coral;
-  color: #fff;
-  padding: 4px 5px;
-  cursor: pointer;
-}
-
+/* 仍然可以保留一些必要的样式，如果有的话 */
 </style>
