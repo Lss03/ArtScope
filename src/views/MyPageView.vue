da1808ae8ccdda37c5fd1e92f202cb35d2c777df
liyunkai13 <474769828@qq.com>
2023-12-30 12:07:24 +0800

    调整购物车和仓库组件的切换效果

--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -1,32 +1,46 @@
 <template>
-  <v-container class="my-container" fluid>
-<!--    <MyPageHeader />-->
-    <MyPageInfo/>
-    <v-divider/>
-    <div>
-      <v-btn-toggle v-model="currentComponent" mandatory >
-        <v-btn value="ShoppingCart">购物车</v-btn>
-        <v-btn value="RepositoryShow">仓库</v-btn>
-      </v-btn-toggle>
+  <div>
+    <MyPageHeader />
+    <div class="content" >
+      <MyPageInfo/>
+      <v-divider></v-divider>
+      <v-tabs
+          color="deep-purple-accent-4"
+          align-tabs="center"
+          grow
+      >
+        <v-tab @click="changeComponent(1)" >购物车</v-tab>
+        <v-tab @click="changeComponent(2)">仓库</v-tab>
+      </v-tabs>
+      <component :is="currentComponent"></component>
     </div>
-    <component :is="currentComponent"></component>
-  </v-container>
+  </div>
 </template>
 
 <script>
-// import MyPageHeader from "@/components/MyPage/MyPageHeader.vue";
+import MyPageHeader from "@/components/MyPage/MyPageHeader.vue";
 import MyPageInfo from "@/components/MyPage/MyPageInfo";
 import ShoppingCart from "@/components/MyPage/ShoppingCart";
 import RepositoryShow from "@/components/MyPage/RepositoryShow";
 
 export default {
-  name: 'MypageView',
+  name: 'MyPageView',
   data: () => ({
-    currentComponent: "ShoppingCart",
+    currentComponent: 'ShoppingCart',
   }),
+  methods: {
+    changeComponent(value) {
+      if (value === 1) {
+        this.currentComponent = 'ShoppingCart'
+      } else if (value === 2) {
+        this.currentComponent = 'RepositoryShow'
+      }
+    }
+  },
+
   components: {
     MyPageInfo,
-    // MyPageHeader,
+    MyPageHeader,
     ShoppingCart,
     RepositoryShow,
   }
@@ -35,10 +49,15 @@ export default {
 
 <style scoped>
 /* 您可以在这里添加样式 */
-.my-container {
+.content {
   display: flex;
   flex-direction: column;
   align-items: flex-start; /* 将子组件在垂直方向上对齐到容器的顶部 */
   justify-content: flex-start; /* 将子组件在水平方向上对齐到容器的左侧 */
+  margin-inline: 10px;
 }
+v-btn-toggle {
+  margin-top: 10px;
+}
+
 </style>
