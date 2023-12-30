720a6a152f7e4a14f5f587505b1f5ca32c133a3c
Lss03 <2782776285@qq.com>
2023-12-30 23:23:02 +0800

    合并详情页面

--- src/components/Home/Gops.vue
+++ src/components/Home/Gops.vue
@@ -1,52 +1,63 @@
 <template>
   <v-container class="container">
-
-    <span class="third-title">风景</span>
-    <div class="photos-container">
-      <v-row no-gutters>
-        <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id" class="item">
-          <img class="item-img" v-if="index < 4" :src="photo.src" alt="" @click="handleButtonClick(photo)">
-        </v-col>
-      </v-row>
+    <div>
+      <span class="third-title">风景</span>
+      <div class="photos-container">
+        <v-row no-gutters>
+          <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id" class="item">
+            <img class="item-img" v-if="index < 4" :src="photo.src" alt="" @click="handleButtonClick">
+          </v-col>
+        </v-row>
+      </div>
     </div>
-    <span class="third-title">猫猫</span>
-    <div class="photos-container">
-      <v-row no-gutters>
-        <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id">
-          <img class="item-img" v-if="(index >= 4)&(index <8)" :src="photo.src" alt="" @click="handleButtonClick(photo)">
-        </v-col>
-      </v-row>
 
-    </div>
-    <span class="third-title">建筑</span>
-    <div class="photos-container">
-      <v-row no-gutters>
-        <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id">
-          <img class="item-img" v-if="(index >=8 )&(index <12)" :src="photo.src" alt="" @click="handleButtonClick(photo)">
-        </v-col>
-      </v-row>
+    <div>
+      <span class="third-title">猫猫</span>
+      <div class="photos-container">
+        <v-row no-gutters>
+          <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id">
+            <img class="item-img" v-if="(index >= 4)&(index <8)" :src="photo.src" alt="" @click="handleButtonClick(photo)">
+          </v-col>
+        </v-row>
+      </div>
 
     </div>
-    <span class="third-title">小狗</span>
-    <div class="photos-container">
-      <v-row no-gutters>
-        <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id">
-          <img class="item-img" v-if="(index >= 12)&(index <16)" :src="photo.src" alt="" @click="handleButtonClick(photo)">
-        </v-col>
-      </v-row>
 
+
+    <div>
+      <span class="third-title">建筑</span>
+      <div class="photos-container">
+        <v-row no-gutters>
+          <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id">
+            <img class="item-img" v-if="(index >=8 )&(index <12)" :src="photo.src" alt="" @click="handleButtonClick(photo)">
+          </v-col>
+        </v-row>
+      </div>
+
+    </div>
+
+    <div>
+      <span class="third-title">小狗</span>
+      <div class="photos-container">
+        <v-row no-gutters>
+          <v-col cols="12" sm="6" md="3" v-for="(photo,index) in photosList" :key="photo.id">
+            <img class="item-img" v-if="(index >= 12)&(index <16)" :src="photo.src" alt="" @click="handleButtonClick(photo)">
+          </v-col>
+        </v-row>
+
+      </div>
     </div>
   </v-container>
 </template>
 
 <style scoped>
 .container{
- margin: 0px;
- padding: 0px;
+  margin: 0;
+  padding: 0;
 }
 .item {
   position: relative;
-  overflow: hidden;
+
 }
 .photos-container {
 
@@ -55,32 +66,32 @@
   border-radius: 15px; /* 圆角 */
   box-shadow: 0 2px 4px rgba(0,0,0,.2); /* 阴影 */
   height: 155px;
-  padding-left: 1%;
+  padding-left:4px;
+
 
 }
 
 .item-img {
-  display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 8px;
   height: 155px;
-  width: 96%;
+  width: 98%;
 
   /* 图片宽度调整为 100% */
   border-radius: 12px;
- /* 图片圆角 */
+  /* 图片圆角 */
   display: block;
   filter: drop-shadow(0px 2px 4px rgba(0, 0, 0,1));
   transition: transform 0.3s ease;
 }
 .item-img:hover {
-  transform: translateX(8px);
+
+  transform: scale(1.05)
 }
 
 .third-title {
-
+  font-family: SimSun, "宋体", sans-serif;
   padding: 0;
-
   font-size: 20px;
   margin-bottom: 8px;
 }
