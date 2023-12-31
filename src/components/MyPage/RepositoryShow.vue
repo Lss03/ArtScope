9b141faf66c87c5c886234e1190a2f707316a35d
liyunkai13 <474769828@qq.com>
2023-12-31 18:55:15 +0800

    个人信息，仓库组件及其初始化逻辑

--- src/components/MyPage/RepositoryShow.vue
+++ src/components/MyPage/RepositoryShow.vue
@@ -1,15 +1,54 @@
 <template>
-<div>
-    <h1>仓库</h1>
-</div>
+  <v-row class="container">
+    <v-col
+        v-for="(item,index) in items"
+        :key="index"
+        class="d-flex child-flex"
+        cols="4"
+    >
+      <v-img
+          width="360"
+          :src=item.src
+          aspect-ratio="1"
+          cover
+          class="bg-grey-lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+          >
+            <v-progress-circular
+                indeterminate
+                color="grey-lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
-  name: "RepositoryShow"
+  name: "RepositoryShow",
+  mounted() {
+    this.$store.dispatch("repositoryInstance/fetchRepoList");
+  },
+  computed: {
+    ...mapState({
+      items: (state) => state.repositoryInstance.items
+    })
+  }
+
 }
 </script>
 
 <style scoped>
-
+.container {
+  align-self: center;
+}
 </style>
