0b5bb54135fe7850a2aa8137463536504f19cc51
liyunkai13 <474769828@qq.com>
2023-12-31 16:45:03 +0800

    创建js模拟仓库数据表及从服务端调用仓库的接口；添加仓库实例状态对象

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import userInstance from "@/store/modules/user";
 import cartInstance from "@/store/modules/cart";
+import repositoryInstance from "@/store/modules/repository";
 import photosInstance from "@/store/modules/photos";
 import xinxiInstance from "@/store/modules/xinxi"
 
@@ -11,6 +12,7 @@ export default new Vuex.Store({
   modules: {
     userInstance,
     cartInstance,
+    repositoryInstance,
     photosInstance,
     xinxiInstance
   }
